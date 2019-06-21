00e2ffb9dd17ca7bd0ae182af7c3b878d3ff0df0
rise_and_shane93 <cdtharper@gmail.com>
2019-06-20 22:19:11 -0400

    first commit

---
+++ src/app/components/Header.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+
+class Header extends Component {
+    render() {
+        return (
+            <header>Hello World!</header>
+        );
+    }
+}
+
+export default Header;
